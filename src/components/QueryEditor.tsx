a002e6e1cb0f8951710697dd85ca64f0623a7115
Christoph Lackner <clackner@gridprotectionalliance.org>
2023-11-13 20:05:24 -0500

    Added Timeout to query on inaction by user

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -11,14 +11,32 @@ import { TextQuery } from './TextQueryEditor';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
-export function QueryEditor({ query, onChange, datasource }: Props) {
+export function QueryEditor({ query, onChange, datasource, onRunQuery }: Props) {
 
   const selectedMode = React.useMemo(() => (query.queryType ?? 'Elements'), [query])
+  const [forceUpdate, setForceUpdate] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+
+      const timeout = setTimeout(() => {
+        onRunQuery();
+      }, 3000);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [forceUpdate]);
 
   const modeChange = React.useCallback((selected: SelectableValue<string>) => { 
     onChange({...query, queryType: ((selected.value ?? 'Elements') as QueryTypes)})
-
+    setForceUpdate(x => !x);
   }, [query,onChange])
+
+  const OnChange = (p: MyQuery) => {
+    onChange(p);
+    setForceUpdate(x => !x);
+  }
 
   return (
     <div className="gf-form" style={{ display: 'flex', flexDirection: 'column' }}>
@@ -31,7 +49,7 @@ export function QueryEditor({ query, onChange, datasource }: Props) {
             </InlineField>
             <InlineField label="Transpose Query" labelWidth={24}>
               <InlineSwitch value={query.transpose ?? false}
-               onChange={(v) => onChange({...query, transpose: (v!.target as HTMLInputElement)!.value === 'on'})}
+               onChange={(v) => OnChange({...query, transpose: !(query.transpose ?? false)})}
                label='used for displaying single value per channel on geo displays etc.'
                showLabel={true}
                />
@@ -41,15 +59,15 @@ export function QueryEditor({ query, onChange, datasource }: Props) {
         <Card>
           <Card.Heading>Meta Data</Card.Heading>
           <Card.Description> 
-            <MetaDataSelector datasource={datasource} onChange={(flds) => onChange({...query, metadataOptions: flds})} query={query.metadataOptions}/>
+            <MetaDataSelector datasource={datasource} onChange={(flds) => OnChange({...query, metadataOptions: flds})} query={query.metadataOptions}/>
           </Card.Description>  
         </Card>
         <Card>
           <Card.Heading>Data Selection</Card.Heading>
           <Card.Description> 
-          {query.queryType === 'Text'? <TextQuery onChange={(t) => onChange({...query, queryText: t})} query={query.queryText}/> : null}
+          {query.queryType === 'Text'? <TextQuery onChange={(t) => OnChange({...query, queryText: t})} query={query.queryText}/> : null}
           {query.queryType === 'Elements' || query.queryType === undefined? <QueryEditorWizard 
-              onChange={(q) => onChange({...query, queryText: q.queryText, parsedQuery: q.parsedQuery})} query={query}
+              onChange={(q) => OnChange({...query, queryText: q.queryText, parsedQuery: q.parsedQuery})} query={query}
               datasource={datasource}/> : null}
            </Card.Description>  
         </Card>
