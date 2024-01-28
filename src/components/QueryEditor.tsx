1bb1b3c3367732f64fe68be9c9f50313bc253b1d
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-01-28 13:27:48 -0600

    Remove timerquery update: Updated text area to requery onblur Elements now requery immediately

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -15,29 +15,23 @@ type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 export function QueryEditor({ query, onChange, datasource, onRunQuery }: Props) {
 
   const selectedMode = React.useMemo(() => (query.queryType ?? 'Elements'), [query])
-  const [forceUpdate, setForceUpdate] = React.useState<boolean>(false);
-
-  React.useEffect(() => {
-
-    const timeout = setTimeout(() => {
-      onRunQuery();
-    }, 3000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [forceUpdate]);
 
   const modeChange = React.useCallback((selected: SelectableValue<string>) => {
     onChange({ ...query, queryType: ((selected.value ?? 'Elements') as QueryTypes) })
-    setForceUpdate(x => !x);
   }, [query, onChange])
 
-  const OnChange = (p: MyQuery) => {
+  const elementsOnChange = (p: MyQuery) => {
     onChange(p);
-    setForceUpdate(x => !x);
+    onRunQuery();
   }
+
+  const textOnChange = (p: MyQuery) => {
+    onChange(p);
+  }
+
+  const textOnBlur = () => {
+    onRunQuery();
+  };
 
   return (
     <FieldSet style={{ display: 'flex', flexDirection: 'column' }}>
@@ -53,7 +47,7 @@ export function QueryEditor({ query, onChange, datasource, onRunQuery }: Props) 
                   </InlineField>
                   <InlineField label="Transpose Query Results" labelWidth={24}>
                     <InlineSwitch value={query.transpose ?? false}
-                      onChange={(v) => OnChange({ ...query, transpose: !(query.transpose ?? false) })}
+                      onChange={(v) => elementsOnChange({ ...query, transpose: !(query.transpose ?? false) })}
                       showLabel={true}
                     />
                   </InlineField>
@@ -65,7 +59,7 @@ export function QueryEditor({ query, onChange, datasource, onRunQuery }: Props) 
             </tr>
             <tr>
               <td>
-                {selectedMode === 'Elements' ? <CommandLevelSelector query={query.commandLevel} onChange={(q) => OnChange({ ...query, commandLevel: q })} /> : null}
+                {selectedMode === 'Elements' ? <CommandLevelSelector query={query.commandLevel} onChange={(q) => elementsOnChange({ ...query, commandLevel: q })} /> : null}
               </td>
             </tr>
           </table>
@@ -73,16 +67,15 @@ export function QueryEditor({ query, onChange, datasource, onRunQuery }: Props) 
       </FieldSet>
       <FieldSet style={{ marginBottom: 16 }}>
         <h3 className="page-heading">Meta Data</h3>
-        <MetaDataSelector datasource={datasource} onChange={(flds) => OnChange({ ...query, metadataOptions: flds })} query={query.metadataOptions} />
+        <MetaDataSelector datasource={datasource} onChange={(fields) => elementsOnChange({ ...query, metadataOptions: fields })} query={query.metadataOptions} />
       </FieldSet>
       <FieldSet>
         <h3 className="page-heading">Data Selection</h3>
-        {query.queryType === 'Text' ? <TextQuery onChange={(t) => OnChange({ ...query, queryText: t })} query={query.queryText} /> : null}
+        {query.queryType === 'Text' ? <TextQuery onChange={(t) => textOnChange({ ...query, queryText: t })} onBlur={textOnBlur} query={query.queryText} /> : null}
         {query.queryType === 'Elements' || query.queryType === undefined ? <QueryEditorWizard
-          onChange={(q) => OnChange({ ...query, queryText: q.queryText, parsedQuery: q.parsedQuery })} query={query}
+          onChange={(q) => elementsOnChange({ ...query, queryText: q.queryText, parsedQuery: q.parsedQuery })} query={query}
           datasource={datasource} /> : null}
       </FieldSet>
     </FieldSet >
   );
-
 }
