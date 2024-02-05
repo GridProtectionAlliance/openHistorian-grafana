8f074669132d23c48549b05935cf32080ab38a20
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-02-05 01:30:33 -0600

    Updated with of query text box for annotations

--- src/components/AnnotationEditor.tsx
+++ src/components/AnnotationEditor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InlineField, TextArea } from '@grafana/ui';
+import { FieldSet, InlineField, TextArea } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from '../datasource';
 import { openHistorianDataSourceOptions, openHistorianQuery } from '../types';
@@ -16,18 +16,16 @@ export function AnnotationEditor({ query, onChange }: Props) {
   }, [query, onChange])
 
   return (
-    <div className="gf-form" style={{ display: 'flex', flexDirection: 'column' }}>
-      <div style={{ width: '100%' }}>
-        <InlineField label="Query" labelWidth={12}>
-          <TextArea
-            value={query.queryText}
-            onChange={(evt: React.ChangeEvent<HTMLTextAreaElement>) => onChange({ ...query, queryText: evt.target.value })}
-            rows={10}
-            style={{ minWidth: '300px', width: '100%' }}
-          />
-        </InlineField>
-      </div>
-    </div>
+    <FieldSet style={{ display: 'flex', flexDirection: 'column' }}>
+      <InlineField label="Query" labelWidth={'auto'} grow={true} shrink={true}>
+        <TextArea
+          value={query.queryText}
+          onChange={(evt: React.ChangeEvent<HTMLTextAreaElement>) => onChange({ ...query, queryText: evt.target.value })}
+          rows={5}
+          width={150}
+        />
+      </InlineField>
+    </FieldSet>
   );
 }
 
